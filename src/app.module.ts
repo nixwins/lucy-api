40e8309b00a6f3b3abfa879e88fceb90a5c04a9a
nixwins <halaulilau@gmail.com>
2021-12-30 18:39:26 +0600

    Added cross-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthModlue } from './auth/auth.module';
-import ProductModule from './products/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
-@Module({ imports: [ProductModule, AuthModlue] })
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+})
 export default class ApplicationModule {}
